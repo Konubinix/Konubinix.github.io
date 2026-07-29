380dee4dfd66e7f44b77f062176ff4f799c24a19
Samuel Loury <konubinixweb@gmail.com>
2026-07-29 21:01:41 +0200

    automatic

--- frameshare/sw.js
+++ frameshare/sw.js
@@ -1,4 +1,4 @@
-const SHELL = 'frameshare-v1';
+const SHELL = 'frameshare-06a5559';
 const KEEP = ['', 'index.html', 'app.js', 'style.css', 'manifest.json', 'icon.svg'];
 self.addEventListener('install', e => e.waitUntil(
     caches.open(SHELL).then(c => c.addAll(KEEP)).then(() => self.skipWaiting())));
